1da5edd01261129534f304f58d487ae0b02bfffa
cshugert <charles.shugert@stonybrook.edu>
2018-05-18 15:32:53 -0400

    added brentq method: issues exist with both

--- meff_analysis.py
+++ meff_analysis.py
@@ -43,32 +43,64 @@ def dispers(psq):
     return np.sqrt(psq + mpisq)
 
 
-def newton_meff(func, Dat, guess, fprime=None, fprime2=None):
-    """ gives me effective mass plot as function of time """
+def newton_meff(func, Dat, guess, fprime=None, fprime2=None, **kwargs):
+    """ gives me effective mass plot as function of time: newton """
     jk_smp = Dat.shape[0]
     T = np.arange(Dat.shape[-1])
     ratio = np.roll(Dat, 1, axis=-1).mean(axis=0)/Dat.mean(axis=0)
     # cv == central value
-    cv_arglist = [(t, ratio) for t in T]
+    cv_arglist = [(t, ratio[t_iter]) for t_iter, t in enumerate(T)]
     cv = [
-        opt.newton(func, guess, args=cv_arg, fprime, fprime2)
+        opt.newton(func, guess, args=cv_arg,
+                   fprime=fprime, fprime2=fprime2)
+        for cv_arg in cv_arglist
     ]
     return cv
 
 
-def newton_meff_mean_var(func, Dat, guess, fprime=None, fprime2=None):
-    """ gives me effective mass plot as function of time with jackknife error """
+def newton_meff_mean_var(func, Dat, guess, fprime=None, fprime2=None, **kwargs):
+    """ 
+    gives me effective mass plot as function of time with jackknife error: newton
+    """
     jk_smp = Dat.shape[0]
     T = np.arange(Dat.shape[-1])
     Blocks = lstats.jk_blocks(Dat, jk_smp, axis=0)
-    cv = newton_meff(func, Dat, guess, fprime, fprime2)
+    cv = newton_meff(func, Dat, guess, fprime, fprime2, **kwargs)
     cvblk = np.array([
-        newton_meff(func, blk, guess, fprime, fprime2)
+        newton_meff(func, blk, guess, fprime, fprime2, **kwargs)
         for blk in Blocks
     ])
     diff = np.array([
         pow(cv - cvb, 2) for cvb in cvblk
     ])
+    err = ((jk_smp - 1.0)/jk_smp)*diff.sum(axis=0)
+    err = np.sqrt(err)
+    return cv, err
+
+
+def bisect_meff(func, Dat, a, b):
+    """ gives me effective mass plot as a function of time: brentq """
+    jk_smp = Dat.shape[0]
+    T = np.arange(Dat.shape[-1])
+    ratio = np.roll(Dat, 1, axis=-1).mean(axis=0)/Dat.mean(axis=0)
+    cv_arglist = [(t, ratio[t_iter]) for t_iter, t in enumerate(T)]
+    cv = [
+        opt.brentq(func, a, b, args=cv_arg)
+        for cv_arg in cv_arglist
+    ]
+    return cv
+
+
+def bisect_meff_mean_var(func, Dat, a, b):
+    """
+    gives me effective mass plot as function of time with jackknife error: brentq
+    """
+    jk_smp = Dat.shape[0]
+    T = np.arange(Dat.shape[-1])
+    Blocks = lstats.jk_blocks(Dat, jk_smp, axis=0)
+    cv = bisect_meff(func, Dat, a, b)
+    cvblk = np.array([bisect_meff(func, blk, a, b) for blk in Blocks])
+    diff = np.array([pow(cv - cvb, 2) for cvb in cvblk])
     err = ((jk_smp - 1.0)/jk_smp)*diff.sum(axis=0)
     err = np.sqrt(err)
     return cv, err
